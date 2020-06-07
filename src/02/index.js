f3e42b028ff8bebc7ae962705142a1e6b3f62272
mfaying <1213560387@qq.com>
2020-06-07 18:12:55 +0800

    feat

--- src/02/index.js
+++ src/02/index.js
@@ -5,7 +5,7 @@ window.data = {
   a: []
 };
 new Observer(window.data);
-new Watcher(window.data, "a", () => {
+new Watcher(window.data.a, "0", () => {
   console.log("Watcher exec");
 });
 // 执行后打印watcher
